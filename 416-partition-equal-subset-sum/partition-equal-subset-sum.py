f362a7e36200f9c5c31b4bef661b2f857ea6854c
Godwin Kibet <156087571+YegonGodwin@users.noreply.github.com>
2025-04-07 20:52:10 +0300

    Time: 486 ms (72.68%) | Memory: 17.9 MB (91.98%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False
+        target = total_sum // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        return dp[target]
+
+solution = Solution()
+nums = [1,5,11,5]
+result = solution.canPartition(nums)
+print(result)
